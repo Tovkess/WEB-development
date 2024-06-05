d5ac9ad792accd775f67eecbf0ab77fbacdf924c
Tovkes Anastasiia <ntovkez@gmail.com>
2024-06-05 08:46:59 +0300

    modified PR9

--- PR-9/app.js
+++ PR-9/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-//TODO: Need to move the logic to separate handler similar to errorHandler
+
 app.use((req, res, next) => {
     logEvent(`${req.method}\t${req.headers.origin}\t${req.url}\n`, 'requestTracing.txt');
     console.log(`${req.method}\t${req.path}`);
@@ -25,7 +25,7 @@ app.get('^/$|/index(.html)?', (req, res) => {
 });
 
 app.get('/about(.html)?', (req, res) => {
-    //throw(new Error('Something went wrong'));
+
     res.sendFile('./views/about.html', { root: __dirname});
 });
 
@@ -37,7 +37,7 @@ app.get('/create(.html)?', (req, res) => {
     res.sendFile('./views/create.html', { root: __dirname});
 });
 
-// /api/products/4
+
 app.use('/api/products', productsRouter);
 app.use('/api/times', require('./routes/times'));
 
